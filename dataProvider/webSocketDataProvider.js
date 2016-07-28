b714ee9ea01bd4d1abecd782bef542fa58b8142b
Jason Coelho <jason_coelho@comcast.com>
2016-07-28 13:26:39 -0400

    Updated websocket component so that it is backward compatible and is able to handle both single url requests as well as json requests, Added documentation

--- dataProvider/webSocketDataProvider.js
+++ dataProvider/webSocketDataProvider.js
@@ -1,3 +1,20 @@
+// webSocketDataProvider.js
+//
+// This component makes a call to the websocket gateway to retrieve data.
+//
+// Input parameters :
+// - webSocketUrl : gateway url
+// - [requestUrl|json] : the websocket gateway can accept a single requestUrl or requestJson or a list of requestUrls and json
+//
+// Example -
+//
+// uiWebSocketDataProvider([requestUrl|json],webSocketUrl)
+//  .then(function(message){
+//
+//          Do logic with the received message here
+//
+// })
+// Jason Coelho
 
 px.import({
   ws:'ws'
@@ -9,34 +26,33 @@ px.import({
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
     // requests contents of a dataservice via a websocket
-    module.exports =  function(urls,websocketUrl,token) {
+    module.exports =  function(requests,websocketUrl) {
 
         return new Promise(function(resolve,reject) {
 
             var returnMsg
 
-            console.log("about to try opening ws for url "+urls + " with token - " + token)
+            console.log("about to try opening ws for url "+requests)
             var mySocket = new ws(websocketUrl)
 
             mySocket.on('open', function() {
                 // console.log('recvd open')            // comment out to prevent noise
-                if (Array.isArray(urls)) {
+                if (Array.isArray(requests)) {
                     returnMsg = []
-                    for (var k = 0; k < urls.length; k++) {
-                        var json = JSON.stringify({url: urls[k], token: token})
+                    for (var k = 0; k < requests.length; k++) {
+                        var json = JSON.stringify(requests[k])
                         mySocket.send(json)
                     }
                 } else {
-                    console.log('single send')
-                    var json = JSON.stringify({url: urls, token: token})
-                    mySocket.send(json)
+                    console.log('single send - ' + requests)
+                    mySocket.send(requests)
                 }
             });
             mySocket.on('message', function(message) {
                 console.log('received: %s', message) // comment out to prevent noise
                 if (Array.isArray(returnMsg) && returnMsg.length >= 0) {
                     returnMsg.push(message)
-                    if (returnMsg.length == urls.length)
+                    if (returnMsg.length == requests.length)
                         mySocket.close()
                 } else {
                     returnMsg = message

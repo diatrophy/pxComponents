569769f3856ff6ac3517e486cc0c16ff59fdaf9e
Jason Coelho <jason_coelho@comcast.com>
2016-09-14 11:24:48 -0400

    selector height is now adjusted to the height of the initial cell it is placed on

--- grid/selector.js
+++ grid/selector.js
@@ -43,7 +43,7 @@ px.import({
                     t['top'] = top
                 })
 
-                lineConfig.y = this.height
+                lineConfig.y = cell.container.h// this.height
 
                 // bottom line
                 imageRenderer.render(image(lineConfig), function (bottom) {
